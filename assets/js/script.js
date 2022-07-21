8e68b2bfd309ea6678ef66a44fdc3e3407596598
Alan <mmckalan@gmail.com>
2022-07-21 14:41:41 +0100

    Fix. Page titles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,40 @@
-// Navigation 
+// Navigation
 $("#toggle").click(function () {
-    $(this).toggleClass('on');
-    $("#resize").toggleClass("active");
+  $(this).toggleClass("on");
+  $("#resize").toggleClass("active");
 });
 
 $("#resize ul li a").click(function () {
-    $(this).toggleClass('on');
-    $("#resize").toggleClass("active");
+  $(this).toggleClass("on");
+  $("#resize").toggleClass("active");
 });
 
 $(".close-btn").click(function () {
-    $(this).toggleClass('on');
-    $("#resize").toggleClass("active");
+  $(this).toggleClass("on");
+  $("#resize").toggleClass("active");
 });
 
 // Nav animation
 TweenMax.from("#brand", 1, {
-    delay: 0.4,
-    y: 10,
-    opacity: 0,
-    ease: Expo.easeInOut
-})
+  delay: 0.4,
+  y: 10,
+  opacity: 0,
+  ease: Expo.easeInOut,
+});
 
-TweenMax.staggerFrom("#menu li a", 1, {
+TweenMax.staggerFrom(
+  "#menu li a",
+  1,
+  {
     delay: 0.4,
     opacity: 0,
-    ease: Expo.easeInOut
-}, 0.1);
+    ease: Expo.easeInOut,
+  },
+  0.1
+);
 
 new WOW().init();
+
+function goToPage(url) {
+  window.location.href = url;
+}
